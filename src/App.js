2b84d81f385a23e04d6467c4e9dbcd58c3b065dc
D-O-C <bezugly.dmitry.work@gmail.com>
2020-12-27 20:11:06 +0200

    Add localization <---RU--->

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 import './App.css';
 // import "bootstrap/dist/css/bootstrap.css";
 import "bootswatch/dist/slate/bootstrap.css";
-import { Navbar, NavItem, Nav, Container, Row, Col} from "react-bootstrap";
+import {Navbar, NavItem, Nav, Container, Row, Col} from "react-bootstrap";
 //slate, solar, superhero
 
 const PLACES = [
     {name: "Харьков", city: "Kharkov"},
     {name: "Свесса", city: "Svesa"},
+    {name: "Дружба", city: "Druzhba"},
     {name: "Краснополье", city: "Krasnopillya"},
     {name: "Сумы", city: "Sumy"},
     {name: "Киев", city: "Kyiv"}
@@ -25,7 +26,7 @@ class WeatherDisplay extends Component {
         const city = this.props.city;
         const URL = "https://api.openweathermap.org/data/2.5/weather?q=" +
             city +
-            "&appid=789d8bf96c25b83d0b02c6bb6f750d34&units=metric";
+            "&appid=789d8bf96c25b83d0b02c6bb6f750d34&lang=ru&units=metric";
         fetch(URL).then(res => res.json()).then(json => {
             this.setState({weatherData: json});
         });
@@ -34,12 +35,19 @@ class WeatherDisplay extends Component {
     render() {
         const weatherData = this.state.weatherData;
         if (!weatherData) return <div>Loading</div>;
+        let city = weatherData.name;
+        if (city === "Svesa") {
+            city = "Свесса";
+        }
+        if (city === "Краснопілля") {
+            city = "Краснополье";
+        }
         const weather = weatherData.weather[0];
         const iconUrl = "https://openweathermap.org/img/w/" + weather.icon + ".png";
         return (
             <div>
                 <h1>
-                    {weather.main} in {weatherData.name}
+                    {weather.description} в городе {city}
                     <img src={iconUrl} alt={weatherData.description}/>
                 </h1>
                 <p>Current: {(weatherData.main.temp).toFixed(0)}°С</p>
@@ -66,15 +74,15 @@ class App extends Component {
             <div>
                 <Navbar>
                     {/*<Navbar.Header>*/}
-                        <Navbar.Brand>
-                            React Simple Weather App
-                        </Navbar.Brand>
+                    <Navbar.Brand>
+                        React Simple Weather App by DoC
+                    </Navbar.Brand>
                     {/*</Navbar.Header>*/}
                 </Navbar>
                 <Container>
                     <Row>
                         <Col md={4} sm={4}>
-                            <h3>Select a city</h3>
+                            <h3>Выберите город из списка ниже</h3>
                             <Nav
                                 className="flex-sm-column"
                                 variant="pills"
